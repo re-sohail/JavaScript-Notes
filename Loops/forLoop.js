74e55ce2d47f47c0f2c2397bfebbcd7fe84fc7ab
sohail-fabtech <sohail.khan@fabtechsol.com>
2025-03-18 07:29:50 +0500

    updated

--- Loops/forLoop.js
+++ Loops/forLoop.js
@@ -5,14 +5,14 @@
 const row = 5;
 let pattern = "";
 
-// for (let i = 1; i <= row; i++) {
-//   for (let j = 1; j <= i; j++) {
-//     pattern += "*";
-//   }
-//   pattern += "\n";
-// }
+for (let i = 1; i <= row; i++) {
+  for (let j = 1; j <= i; j++) {
+    pattern += "*";
+  }
+  pattern += "\n";
+}
 
-// console.log(pattern);
+console.log(pattern);
 
 // console.log("------------- Triangle Pattern (Reverse Order) -------------");
 
